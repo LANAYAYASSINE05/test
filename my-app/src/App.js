2559299c23f60b7055db7ca4af62bd541386e90f
LANAYAYASSINE05 <153106164+LANAYAYASSINE05@users.noreply.github.com>
2024-11-07 22:49:21 +0100

    new commit

---
+++ my-app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  function yassine(){
+    const name="curva sud";
+    return name ;
+  }
+  return (
+    <div className="App">
+      <h1>hello world</h1>
+      <p>{yassine()}</p>
+    
+    </div>
+  );
+}
+
+export default App;
